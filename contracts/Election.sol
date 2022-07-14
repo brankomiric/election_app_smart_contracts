39375e1f60370117d31dfde2f0af08767c2d4f2a
Branko <branko.miric.ns@gmail.com>
2022-07-14 13:15:09 +0200

    finished election contract + tests

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -3,19 +3,49 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-contract Election {
-    mapping(address => uint256) votes;
-    mapping(address => bool) voters;
-    uint256 electeeCount;
+struct Electee {
+    address electeeAddr;
+    uint256 score;
+}
 
-    constructor(uint256 electees) {
-        electeeCount = electees;
-    }
+contract Election {
+    mapping(address => uint256) private votes;
+    mapping(address => bool) private voters;
+    address[] private electeeAddresses;
 
     function vote(address candidate) external {
         require(!voters[msg.sender], "You're allowed to a single vote");
 
         votes[candidate] += 1;
         voters[msg.sender] = true;
+
+        if (!isIn(electeeAddresses, candidate)) {
+            electeeAddresses.push(candidate);
+        }
+    }
+
+    function getScoreboard() external view returns (Electee[] memory) {
+        Electee[] memory scoreboard = new Electee[](electeeAddresses.length);
+        for (uint256 i = 0; i < electeeAddresses.length; i++) {
+            Electee memory e = Electee({
+                electeeAddr: electeeAddresses[i],
+                score: votes[electeeAddresses[i]]
+            });
+            scoreboard[i] = e;
+        }
+        return scoreboard;
+    }
+
+    function isIn(address[] memory list, address a)
+        private
+        pure
+        returns (bool)
+    {
+        for (uint256 i = 0; i < list.length; i++) {
+            if (list[i] == a) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- test/election.test.js
+++ test/election.test.js
@@ -4,9 +4,8 @@ const { utils, Contract } = require("ethers");
 
 const abi = [
   "function vote(address candidate)",
+  "function getScoreboard() view returns (tuple(address electeeAddr, uint256 score)[] electees)"
 ];
-
-const electeesCount = 10;
 
 describe("Election", async () => {
   it("Should be possible to vote once", async () => {
@@ -34,11 +33,46 @@ describe("Election", async () => {
       contract.vote(signers[5].address, { gasLimit: 200000 })
     ).rejectedWith("You're allowed to a single vote");
   });
+
+  it("Should return the scoreboard", async() => {
+    const contractAddr = await deployContract();
+
+    const signers = await ethers.getSigners();
+
+    const contractInstance1 = await initContract(signers[1], contractAddr);
+
+    // Vote for signers[5]
+    let tx = await contractInstance1.vote(signers[5].address, { gasLimit: 200000 });
+    await tx.wait(1);
+
+    const contractInstance2 = await initContract(signers[2], contractAddr);
+
+    // Vote for signers[6]
+    tx = await contractInstance2.vote(signers[6].address, { gasLimit: 200000 });
+    await tx.wait(1);
+
+    const contractInstance3 = await initContract(signers[3], contractAddr);
+
+    // Another vote for signers[5]
+    tx = await contractInstance3.vote(signers[5].address, { gasLimit: 200000 });
+    await tx.wait(1);
+
+    const scoreboard = await contractInstance1.getScoreboard();
+
+    // Address signers[5] should have 2 votes
+    expect(scoreboard[0].electeeAddr).to.eq(signers[5].address);
+    expect(scoreboard[0].score.toString()).to.eq("2");
+
+    // Address signers[6] should have 1 vote
+    expect(scoreboard[1].electeeAddr).to.eq(signers[6].address);
+    expect(scoreboard[1].score.toString()).to.eq("1");
+
+  });
 });
 
 const deployContract = async () => {
   const Contract = await ethers.getContractFactory("Election");
-  const contract = await Contract.deploy(electeesCount);
+  const contract = await Contract.deploy();
   await contract.deployed();
   return contract.address;
 };
